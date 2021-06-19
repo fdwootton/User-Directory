d22041dc1a8d5b84d79f1b04982059dee52fbc76
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-19 17:10:23 -0500

    fixed error, added variable for api call

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export default {
-    //get 20 random employees
-  searchEmployee: function() {
-    return axios.get('https://randomuser.me/api/?inc=picture,name,phone,email&results=20');
-  }
-};
+let getRequest = {
+    //retrieve 20 random employees
+    searchEmployee: function() {
+        return axios.get('https://randomuser.me/api/?inc=picture,name,phone,email&results=20');
+      }
+}
+export default getRequest
