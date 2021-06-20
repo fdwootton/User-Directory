9161610574ba62dd0d95bee43f1a2734a10f94ae
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-19 19:50:13 -0500

    added dummy data to employee table

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -5,30 +5,30 @@ const EmployeeTable = (props) => {
 <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">NAME</th>
+      <th scope="col">EMAIL</th>
+      <th scope="col">PHONE</th>
+      <th scope="col">DOB</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">Mark Otto</th>
+      <td>markotto@test.com</td>
+      <td>555-555-5555</td>
+      <td>02-05-1982</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">Jacob Thornton</th>
+      <td>jthornton@test.com</td>
+      <td>314-555-1234</td>
+      <td>12-30-1999</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">Lisa Bird</th>
+      <td>lisabird@test.com</td>
+      <td>636-555-4321</td>
+      <td>09-13-1967</td>
     </tr>
   </tbody>
 </table>
