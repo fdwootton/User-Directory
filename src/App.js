f9ed6c95208bdbc08d0f787801b8b242bcd0a859
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-19 19:38:09 -0500

    added dummy EmployeeTable to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
+import EmployeeTable from "./components/EmployeeTable";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <Header />
       <SearchBar />
+      <EmployeeTable />
     </>
   );
 };
