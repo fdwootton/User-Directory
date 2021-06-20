5f487fc3b2fb0a9d54f171be8538fc126b4af76f
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-20 16:54:03 -0500

    edited query for api call

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let apiCall = {
     //retrieve 20 random users
       search: function() {
-      return axios.get('https://randomuser.me/api/?inc=picture,name,phone,email,login&results=40&nat=us');
+      return axios.get('https://randomuser.me/api/?inc=picture,name,phone,email&results=40&nat=us');
     }
 }
 export default apiCall;
