0e6c10af7fb85072dc155b2590b47255835860c2
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-19 19:12:31 -0500

    added SearchBar component to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 
 const App = () => {
   return (
     <>
       <Header />
+      <SearchBar />
     </>
   );
 };
