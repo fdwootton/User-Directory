2c96400c533fea0bae10d33a274c213d0060d8e5
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-20 14:11:51 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import "./App.css";
 import SearchResultContainer from "./components/SearchResultContainer";
 
-
-
 const App = () => {
   return (
-    <>
       <SearchResultContainer />
-    </>
   );
 };
 
 export default App;
+
